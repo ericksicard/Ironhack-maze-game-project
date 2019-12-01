493d6688659c2c4f835e7a9cea024a5466b067db
Erick Sicard <esrfree@gmail.com>
2019-12-01 01:46:08 -0500

    maze generation

---
+++ cell.js
@@ -0,0 +1,73 @@
+
+/* eslint-disable linebreak-style */
+class Cell {
+    constructor(i, j) {
+        this.i = i;
+        this.j = j;
+        this.walls = [true, true, true, true]; // [ top, right, botton, left ]
+        this.visited = false;
+
+        // draw the cells of the grid
+        this.show = function (ctx) {
+            // cell location
+            let x = this.i * w;
+            let y = this.j * w;
+            // draw cell
+            if (this.walls[0]) { // top wall
+                ctx.beginPath(); // Start a new path
+                ctx.moveTo(x, y); // Move the pen to the line starting point
+                ctx.lineTo(x + w, y); // Draw a line to the ending point
+                ctx.stroke(); // Render the path            
+            }
+            if (this.walls[1]) { // rigth wall
+                ctx.beginPath();
+                ctx.moveTo(x + w, y);
+                ctx.lineTo(x + w, y + w);
+                ctx.stroke();
+            }
+            if (this.walls[2]) { // bottom wall
+                ctx.beginPath();
+                ctx.moveTo(x + w, y + w);
+                ctx.lineTo(x, y + w);
+                ctx.stroke();
+            }
+            if (this.walls[3]) { // left wall
+                ctx.beginPath();
+                ctx.moveTo(x, y + w);
+                ctx.lineTo(x, y);
+                ctx.stroke();
+            }
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = 'white';
+            ctx.font = '10px serif';
+            ctx.fillStyle = 'white';
+            // color the visited cell
+            if (this.visited) {
+                ctx.fillStyle = 'red';
+                ctx.fillText(`${this.visited}`, x + 15, y + 25);
+                //ctx.fillRect( x, y , w, w );
+            }
+        };
+
+        // determine if the cell's neighbors have been visited
+        this.checkNeigbors = function () {
+            let neighbors = [];
+            let topNeighbor = grid[index(this.i, this.j - 1)];
+            let rightNeighbor = grid[index(this.i + 1, this.j)];
+            let bottomNeighbor = grid[index(this.i, this.j + 1)];
+            let leftNeighbor = grid[index(this.i - 1, this.j)];
+            if (topNeighbor && !topNeighbor.visited)
+                neighbors.push(topNeighbor);
+            if (rightNeighbor && !rightNeighbor.visited)
+                neighbors.push(rightNeighbor);
+            if (bottomNeighbor && !bottomNeighbor.visited)
+                neighbors.push(bottomNeighbor);
+            if (leftNeighbor && !leftNeighbor.visited)
+                neighbors.push(leftNeighbor);
+            if (neighbors.length > 0) {
+                let random = Math.floor(Math.random() * neighbors.length);
+                return neighbors[random];
+            }
+        };
+    }
+}
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 var cols, rows;
 var w= 50;
-var grid = [];
+var grid= [];
+var stack = [];
 
 let canva = {
     width: 500,
@@ -23,9 +24,7 @@ function setup() {
     buildGrid();
 
     // select random cells statrting at position (0,0)
-    let current = grid[0];
-    current.visited = true;
-    mazePath( current );
+    mazePath( grid[0] );
     
     // draw the matrix and maze
     drawGrid( ctx );
@@ -42,19 +41,27 @@ function buildGrid () {
     }
 }
 
-function mazePath ( current ) {
+function mazePath ( current ) {    
+    current.visited = true;
     let next = current.checkNeigbors();
+
     if (next) {
         next.visited = true;
+        stack.push( current );
         removeWalls( current, next );
         current = next;
         mazePath( current );
-    } else return;
+    } 
+    else if( stack.length > 0 ) {
+        current = stack.pop();
+        mazePath( current );
+    } 
+    else return; 
 }
 
 function drawGrid( ctx ) {    
     // draw the background
-    ctx.fillRect(0, 0, 500, 500);    
+    ctx.fillRect(0, 0, canva.width, canva.height);    
 
     // display the grid
     for ( let i = 0; i < grid.length; i++) {
@@ -100,79 +107,6 @@ function index( i, j ) {
     }
 }
 
-/* *********************************** */
- 
-function Cell( i, j ) {
-    this.i = i;
-    this.j = j;
-    this.walls = [true, true, true, true]; // [ top, right, botton, left ]
-    this.visited = false;
-    
-    // draw the cells of the grid
-    this.show = function( ctx ) {
-        // cell location
-        let x = this.i * w;
-        let y = this.j * w;
-        
-        // draw cell
-        if ( this.walls[0] ) {     // top wall
-            ctx.beginPath();       // Start a new path
-            ctx.moveTo(x, y);      // Move the pen to the line starting point
-            ctx.lineTo(x + w, y);  // Draw a line to the ending point
-            ctx.stroke();          // Render the path            
-        }
-        if ( this.walls[1] ) {     // rigth wall
-            ctx.beginPath();       
-            ctx.moveTo( x + w, y );      
-            ctx.lineTo( x + w, y + w );  
-            ctx.stroke();
-        }
-        if ( this.walls[2] ) {     // bottom wall
-            ctx.beginPath();       
-            ctx.moveTo( x + w, y + w );      
-            ctx.lineTo( x ,    y + w );  
-            ctx.stroke();
-        }
-        if ( this.walls[3] ) {     // left wall
-            ctx.beginPath();       
-            ctx.moveTo( x ,    y + w  );      
-            ctx.lineTo( x,     y );  
-            ctx.stroke();
-        }
-
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = 'white';
-        ctx.font = '10px serif';
-        ctx.fillStyle = 'white';
-        
-        // color the visited cell
-        if ( this.visited ) {
-            ctx.fillStyle = 'red';
-            ctx.fillText(`${this.visited}`, x + 15, y + 25);
-            //ctx.fillRect( x, y , w, w );
-        }
-    };
-
-    // determine if the cell's neighbors have been visited
-    this.checkNeigbors = function() {
-        let neighbors = [];
-
-        let topNeighbor    = grid[ index( this.i,     this.j - 1 ) ];
-        let rightNeighbor  = grid[ index( this.i + 1, this.j ) ];
-        let bottomNeighbor = grid[ index( this.i,     this.j + 1 ) ];
-        let leftNeighbor   = grid[ index( this.i - 1, this.j ) ];
-
-        if ( topNeighbor && !topNeighbor.visited ) neighbors.push( topNeighbor );
-        if ( rightNeighbor && !rightNeighbor.visited ) neighbors.push( rightNeighbor );
-        if ( bottomNeighbor && !bottomNeighbor.visited ) neighbors.push( bottomNeighbor );
-        if ( leftNeighbor && !leftNeighbor.visited ) neighbors.push( leftNeighbor );
-
-        if ( neighbors.length > 0 ) {
-            let random = Math.floor( Math.random() * neighbors.length );
-            return neighbors[random];
-        }
-    };
-}
 
 // *************************************************** //
 
