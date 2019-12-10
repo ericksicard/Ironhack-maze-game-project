c17c71ca166a276b4f58764973d0f4f65c43715e
ERICK SICARD <esrfree@gmail.com>
2019-12-09 20:38:53 -0500

    hunter moving randomly

--- hunter.js
+++ hunter.js
@@ -19,41 +19,193 @@ class Hunter {
 
         // player moves
         this.moves = () => {
-            // implement a random selection to move the hunter
-            // CODE GOES HERE
+
             //Hunter inside the grid
             if ( this.posY >= w/2 && this.posX <= canva.width - w/2 && 
                 this.posY <= canva.height - w/2 && this.posX >= w/2) {
+                
                 // moves up
-                if ( (!grid[this.idx].walls[0] && this.hunted !== this.idx - cols) ||
-                    ( grid[this.idx].walls[1] && grid[this.idx].walls[2] && grid[this.idx].walls[3]) ) {
+                if ( grid[this.idx].walls[1] && grid[this.idx].walls[2] && grid[this.idx].walls[3] ) {
                     this.hunted = this.idx;
-                    this.posY -= cellWith;
-                    this.idx -= cols;
+                    this.movesUp();
                 }
                 // moves right
-                else if ( (!grid[this.idx].walls[1] && this.hunted !== this.idx + 1) ||
-                    ( grid[this.idx].walls[0] && grid[this.idx].walls[2] && grid[this.idx].walls[3])) {
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[2] && grid[this.idx].walls[3] ) {
                     this.hunted = this.idx;
-                    this.posX += cellWith;
-                    this.idx += 1;
+                    this.movesRight();
                 }
                 // moves down
-                else if ( (!grid[this.idx].walls[2] && this.hunted !== this.idx + cols) ||
-                    ( grid[this.idx].walls[0] && grid[this.idx].walls[1] && grid[this.idx].walls[3]) ) {
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[1] && grid[this.idx].walls[3] ) {
                     this.hunted = this.idx;
-                    this.posY += cellWith;
-                    this.idx += cols;
+                    this.movesDown();
                 }
                 // moves left
-                else if ( (!grid[this.idx].walls[3] && this.hunted !== this.idx - 1) ||
-                    ( grid[this.idx].walls[0] && grid[this.idx].walls[1] && grid[this.idx].walls[2]) ) {
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[1] && grid[this.idx].walls[2] ) {
                     this.hunted = this.idx;
-                    this.posX -= cellWith;
-                    this.idx -= 1;
+                    this.movesLeft();
                 }
 
+                // moves left or down
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[1] ) {
+                    if ( this.hunted !== this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        this.movesLeft();
+                    }
+                    else if ( this.hunted !== this.idx + cols ) {
+                        this.hunted = this.idx;
+                        this.movesDown();
+                    }
+                }
+                // moves left or up
+                else if ( grid[this.idx].walls[1] && grid[this.idx].walls[2] ) {
+                    if ( this.hunted !== this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        this.movesLeft();
+                    }
+                    else if ( this.hunted !== this.idx - cols ) {
+                        this.hunted = this.idx;
+                        this.movesUp();
+                    }
+                }
+                // moves right or up
+                else if ( grid[this.idx].walls[2] && grid[this.idx].walls[3] ) {
+                    if ( this.hunted !== this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        this.movesRight();
+                    }
+                    else if ( this.hunted !== this.idx - cols ) {
+                        this.hunted = this.idx;
+                        this.movesUp();
+                    }
+                }
+                // moves right or down
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[3] ) {
+                    if ( this.hunted !== this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        this.movesRight();
+                    }
+                    else if ( this.hunted !== this.idx + cols ) {
+                        this.hunted = this.idx;
+                        this.movesDown();
+                    }
+                }
+                // moves up or down
+                else if ( grid[this.idx].walls[1] && grid[this.idx].walls[3] ) {
+                    if ( this.hunted !== this.idx - cols ) {
+                        this.hunted = this.idx;
+                        this.movesUp();
+                    }
+                    else if ( this.hunted !== this.idx + cols ) {
+                        this.hunted = this.idx;
+                        this.movesDown();
+                    }
+                }
+                // moves right or left
+                else if ( grid[this.idx].walls[0] && grid[this.idx].walls[2] ) {
+                    if ( this.hunted !== this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        this.movesRight();
+                    }
+                    else if ( this.hunted !== this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        this.movesLeft();
+                    }
+                }
+                // moves right or left or down
+                else if ( grid[this.idx].walls[0] ) {
+                    let r = this.randomMove();
+                    if ( this.hunted === this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesLeft();
+                        else if ( r == 1 ) this.movesDown();
+                    }
+                    else if ( this.hunted == this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesRight();
+                        else if ( r == 1 ) this.movesDown();
+                    }
+                    else if ( this.hunted == this.idx + cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesLeft();
+                        else if ( r == 1 ) this.movesRight();
+                    }
+                }
+                // moves up or down or left
+                else if ( grid[this.idx].walls[1] ) {
+                    let r = this.randomMove();
+                    if ( this.hunted == this.idx - cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesLeft();
+                        else if ( r == 1 ) this.movesDown();
+                    }
+                    else if ( this.hunted == this.idx + cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesLeft();
+                        else if ( r == 1 ) this.movesUp();
+                    }
+                    else if ( this.hunted == this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesDown();
+                        else if ( r == 1 ) this.movesUp();
+                    }
+                }
+                // moves right or left or up
+                else if ( grid[this.idx].walls[2] ) {
+                    let r = this.randomMove();
+                    if ( this.hunted == this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesLeft();
+                        else if ( r == 1 ) this.movesUp();
+                    }
+                    else if ( this.hunted == this.idx - 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesRight();
+                        else if ( r == 1 ) this.movesUp();
+                    }
+                    else if ( this.hunted == this.idx - cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesRight();
+                        else if ( r == 1 ) this.movesLeft();
+                    }
+                }
+                // moves up or down or right
+                else if ( grid[this.idx].walls[3] ) {
+                    let r = this.randomMove();
+                    if ( this.hunted == this.idx - cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesRight();
+                        else if ( r == 1 ) this.movesDown();
+                    }
+                    else if ( this.hunted == this.idx + cols ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesRight();
+                        else if ( r == 1 ) this.movesUp();
+                    }
+                    else if ( this.hunted == this.idx + 1 ) {
+                        this.hunted = this.idx;
+                        if ( r == 0 ) this.movesUp();
+                        else if ( r == 1 ) this.movesDown();
+                    }
+                }
             }       
         }
+
+        this.movesUp = () => {
+            this.posY -= cellWith;
+            this.idx -= cols;
+        }
+        this.movesRight = () => {
+            this.posX += cellWith;
+            this.idx += 1;
+        }
+        this.movesDown = () => {
+            this.posY += cellWith;
+            this.idx += cols;
+        }
+        this.movesLeft = () => {
+            this.posX -= cellWith;
+            this.idx -= 1;
+        }
+        this.randomMove = () => Math.floor( Math.random() * 2 );
     }
 }
